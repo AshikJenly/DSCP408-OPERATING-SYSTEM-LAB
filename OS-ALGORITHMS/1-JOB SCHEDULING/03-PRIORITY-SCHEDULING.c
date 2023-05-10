8b3f5d8085e0778a0f44fe533066c40c302c9937
ASHIK JENLY <116492348+AshikJenly@users.noreply.github.com>
2023-05-10 16:44:47 +0530

    added job scheduled

---
+++ OS-ALGORITHMS/1-JOB SCHEDULING/03-PRIORITY-SCHEDULING.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+
+typedef struct process{
+    char name[30];
+    int wait,burst,priority;
+}process;
+
+process PRCS[10];
+int N,TOTAL=0;
+int i,j;//For iteration
+void read_process();
+void calculate_waiting_time();
+void print_details();
+
+void main(){
+    read_process();
+    calculate_waiting_time();
+    print_details();
+
+    printf("\nTOTAL : %d",TOTAL);
+    printf("\nAVERAGE %0.2f :",(TOTAL/N*1.0));
+    return;
+}
+void read_process()
+{
+    
+    printf("\nEnter number of process : ");
+    scanf("%d",&N);
+
+    for(i=0;i<N;i++)
+    {
+        
+        printf("Process %d\n\tEnter process name : ",i);
+        scanf("%s",PRCS[i].name);
+        printf("\tEnter the burst time : ");
+        scanf("%d",&PRCS[i].burst);
+        printf("\tEnter the Priority : ");
+        scanf("%d",&PRCS[i].priority);
+
+
+    }
+    
+}
+void calculate_waiting_time()
+{
+    process temp;
+    for(i=0;i<N;i++)
+        for(j=0;j<N-i-1;j++)
+            if(PRCS[j].priority>PRCS[j+1].priority)
+            {
+                temp=PRCS[j];
+                PRCS[j]=PRCS[j+1];
+                PRCS[j+1]=temp;
+                
+            }
+        
+    for(i=0;i<N;i++)
+    {
+        if(i==0)
+            PRCS[i].wait=0;
+        else
+            PRCS[i].wait = PRCS[i-1].wait + PRCS[i-1].burst;
+        TOTAL += PRCS[i].wait;
+    }
+}
+void print_details()
+{     
+    printf("\nNAME\t\tBURST\tPRIORITY\tWAIT");
+    for(i=0;i<N;i++)
+    {
+        printf("\n%s\t\t%d\t\t%d\t\t%d",PRCS[i].name,PRCS[i].burst,PRCS[i].priority,PRCS[i].wait);
+
+    }
+}
